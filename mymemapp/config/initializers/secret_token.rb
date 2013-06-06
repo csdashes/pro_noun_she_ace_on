7f094bd6fe71913ad1de1a3a0ea74a3304771ea7
nop33 <hliastrich@yahoo.gr>
2013-06-06 17:12:08 +0300

    Upload Rails project

---
+++ mymemapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymemapp::Application.config.secret_token = 'c87bff7cd27502c1858be55f00461b585a440f888b79e54e0c2e32a7e74d13d869bbf67bfda1e8d5df2f9454983f2bd75ccbb1378ac8a2dc76f07114b3f2f86b'
